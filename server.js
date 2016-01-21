40efe2be7a653ecc946f38b84d6629b05a9ed25d
Rafael Angelo <rlangelo@wpi.edu>
2016-01-21 13:08:58 -0500

    Try again

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require('express');
 var path = require('path');
-var multer = require('multer');
 
 var app = express();
-var storage = multer.diskStorage({
-	destination: function (req, file, callback) {
-		callback(null, './files');
-	},
-	filename: function (req, file, callback) {
-		callback(null, file.fieldname + '-' + Date.now());
-	}
-});
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/', function(req, res) {
-  		res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+//app.use('/', function(req, res) {
+ // 		res.sendFile(path.join(__dirname, '/public/index.html'));
+//});
 
 app.listen(port, function() {
   console.log('App is listening on port ' + port);
